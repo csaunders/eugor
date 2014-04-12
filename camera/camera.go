80213148c728911fbd0a29e493ac458cfcc4da06
Chris Saunders <chris.saunders@shopify.com>
2014-04-12 15:06:14 +0000

    Properly position the character in the viewport
    Forgive me for my sins. Thankfully I can test this so once tests
    are in place refactoring should be easy peasy.

--- camera/camera.go
+++ camera/camera.go
@@ -12,40 +12,53 @@ type Drawable interface {
 }
 
 func CameraDraw(field dungeon.TileMap, focus Drawable) (focusDrawPoint, fieldStartPoint dungeon.Point, meta string) {
+	origin := dungeon.MakePoint(0, 0)
 	position := dungeon.MakePoint(focus.X(), focus.Y())
 	w, h := termbox.Size()
 	screenEnd := dungeon.MakePoint(w, h)
 	screenMid := dungeon.MakePoint(screenEnd.X/2, screenEnd.Y/2)
 	endField := dungeon.MakePoint(field.Width, field.Height)
-	if position.LessThan(screenMid) {
-		meta = "Less than midscreen"
+
+	// TODO: Refactor the shit out of this, it maeks me sad
+	switch {
+	// Upper Left
+	case position.Minus(screenMid).LessThan(origin):
 		focusDrawPoint = position
-		fieldStartPoint = dungeon.MakePoint(0, 0)
-	} else if position.GreaterThan(endField.Minus(screenMid)) {
-		meta = "Greater than field size"
-		focusDrawPoint = screenEnd.Plus(position.Minus(endField))
-		fieldStartPoint = endField.Minus(screenEnd)
-	} else {
-		meta = "Somewhere in between"
+		fieldStartPoint = origin
+	// Upper Right
+	case position.Y-screenMid.Y < 0 && endField.X-(position.X+screenMid.X) < 0:
+		focusDrawPoint = dungeon.MakePoint(position.X+screenEnd.X-endField.X, position.Y)
+		fieldStartPoint = dungeon.MakePoint(endField.X-screenEnd.X, 0)
+	// Lower Left
+	case position.X-screenMid.X < 0 && endField.Y-(position.Y+screenMid.Y) < 0:
+		focusDrawPoint = dungeon.MakePoint(position.X, position.Y+screenEnd.Y-endField.Y)
+		fieldStartPoint = dungeon.MakePoint(0, endField.Y-screenEnd.Y)
+	// Lower Right
+	case endField.Y-(position.Y+screenMid.Y) < 0 && endField.X-(position.X+screenMid.X) < 0:
+		focusDrawPoint = dungeon.MakePoint(position.X+screenEnd.X-endField.X, position.Y+screenEnd.Y-endField.Y)
+		fieldStartPoint = dungeon.MakePoint(endField.X-screenEnd.X, endField.Y-screenEnd.Y)
+	// Upper Edge of Map
+	case position.Y-screenMid.Y < 0:
+		focusDrawPoint = dungeon.MakePoint(screenMid.X, position.Y)
+		fieldStartPoint = dungeon.MakePoint(position.X-screenMid.X, 0)
+	// Middle Left Edge of Map
+	case position.X-screenMid.X < 0:
+		focusDrawPoint = dungeon.MakePoint(position.X, screenMid.Y)
+		fieldStartPoint = dungeon.MakePoint(0, position.Y-screenMid.Y)
+	// Middle Right Edge of Map
+	case endField.X-(position.X+screenMid.X) < 0:
+		focusDrawPoint = dungeon.MakePoint(position.X+screenEnd.X-endField.X, screenMid.Y)
+		fieldStartPoint = dungeon.MakePoint(endField.X-screenEnd.X, position.Y-screenMid.Y)
+	// Bottom Edge of Map
+	case endField.Y-(position.Y+screenMid.Y) < 0:
+		focusDrawPoint = dungeon.MakePoint(screenMid.X, position.Y+screenEnd.Y-endField.Y)
+		fieldStartPoint = dungeon.MakePoint(position.X-screenMid.X, endField.Y-screenEnd.Y)
+	// Anywhere in the Middle of the Map
+	default:
 		focusDrawPoint = screenMid
-		if position.X < screenMid.X {
-			focusDrawPoint.X = position.X
-		}
-		if position.Y < screenMid.Y {
-			focusDrawPoint.Y = position.Y
-		}
-		fieldStartPoint = position.Minus(screenMid)
+		fieldStartPoint = dungeon.MakePoint(position.X-screenMid.X, position.Y-screenMid.Y)
 	}
-	if fieldStartPoint.X < 0 {
-		fieldStartPoint.X = 0
-	} else if fieldStartPoint.X+w > endField.X {
-		fieldStartPoint.X = endField.X - w
-	}
-	if fieldStartPoint.Y < 0 {
-		fieldStartPoint.Y = 0
-	} else if fieldStartPoint.Y+h > endField.Y {
-		fieldStartPoint.Y = endField.Y - h
-	}
+
 	for x := 0; x < screenEnd.X; x++ {
 		for y := 0; y < screenEnd.Y; y++ {
 			field.DrawProjection(x, y, fieldStartPoint.X+x, fieldStartPoint.Y+y)
